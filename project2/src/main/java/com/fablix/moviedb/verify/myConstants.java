05ce2b63380cf5113f3992c9db9dadcd8cecbe61
yuanfanz <yuanfanz@uci.edu>
2016-04-30 14:54:06 -0700

    verify bots

---
+++ project2/src/main/java/com/fablix/moviedb/verify/myConstants.java
@@ -0,0 +1,10 @@
+package com.fablix.moviedb.verify;
+
+class myConstants {
+
+
+    public static final String SITE_KEY ="6LfyaR4TAAAAAD1PCJsZ3WAkv3JVgoGp7YSov1qo";
+
+    public static final String SECRET_KEY ="6LfyaR4TAAAAANfGwpJhSdkaXQMgPnIvVe6HOk6u";
+
+}
