881d0c044776c03e08beb2fba8f7684da340e42c
yuanfanz <yuanfanz@uci.edu>
2016-05-02 11:49:01 -0700

    employee object

---
+++ project2/src/main/java/com/fablix/moviedb/model/Employees.java
@@ -0,0 +1,27 @@
+package com.fablix.moviedb.model;
+
+public class Employees {
+	private String email;
+	private String password;
+	private String fullname;
+
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	
+}
