7632cc08f9e3904a0a31f361bd720d6082f215af
yuanfanz <yuanfanz@uci.edu>
2016-06-21 10:57:08 -0700

    .

--- project3/src/main/java/com/fablix/moviedb/db/dbConnection.java
+++ project3/src/main/java/com/fablix/moviedb/db/dbConnection.java
@@ -5,7 +5,7 @@ public class dbConnection {
 	
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/moviedb";
 	private static final String USER = "root";
-	private static final String PASSWORD = "1993zhangtianle";
+	private static final String PASSWORD = "123456";
 	
 	private static Connection connection = null;
 		
